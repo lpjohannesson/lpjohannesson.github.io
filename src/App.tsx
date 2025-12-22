f686d42cb070fbfbd87b12c4348e8fc9c5f47c4c
lpjohannesson <devcrossfrog@gmail.com>
2025-12-21 23:40:58 -0500

    Resume flex

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,11 @@ function PageContainer(props: any) {
               gap: "8px"
             }}>
             <Box sx={{ flex: "1" }}>
-              <motion.div style={{ height: "100%" }}
+              <motion.div style={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column"
+              }}
                 initial={{ opacity: 0, transform: "translate(0, 16px)" }}
                 animate={{ opacity: 1, transform: "none" }}
                 transition={{ duration: 0.5 }}>
@@ -112,7 +116,7 @@ function HomePage() {
     <PageContainer>
       <Typography variant="subtitle1">Hello! I'm a software and game developer from Canada.</Typography>
       <Typography variant="subtitle1">Here is my resume <Link href="https://docs.google.com/document/d/1lwQBg_FuuACZvSnHIJ3o34GddLe9ElhGt7qHqYBQVyU/edit?usp=sharing">(link)</Link>:</Typography>
-      <iframe style={{ height: "600px", border: "none" }} width="100%" src="https://docs.google.com/document/d/e/2PACX-1vRambdM9vglKdwpvNl2tgXALH8ikaGPPuB8nc6ecGKnu_721nM8vFL-8BVDFwsLapRfwDgozmg8atUw/pub?embedded=true"></iframe>
+      <iframe style={{ flex: "1", border: "none" }} width="100%" src="https://docs.google.com/document/d/e/2PACX-1vRambdM9vglKdwpvNl2tgXALH8ikaGPPuB8nc6ecGKnu_721nM8vFL-8BVDFwsLapRfwDgozmg8atUw/pub?embedded=true"></iframe>
     </PageContainer>
   )
 }
