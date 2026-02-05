80a8541769dfd2a612c652bbf1544e10b64210be
Leif Johannesson <lp.johannesson@gmail.com>
2026-02-05 02:30:42 -0500

    Remove typography

--- src/ResumesPage.tsx
+++ src/ResumesPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { useState } from "react";
 import { Outlet } from "react-router";
 import FadeContainer from "./FadeContainer";
